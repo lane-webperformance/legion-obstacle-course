a8b5d480586e1aef9d2c599eafc31c9cbd89ceb9
Christopher Lane Hinson <lane@webperformance.com>
2018-02-10 13:34:55 -0500

    Expose the http module by default, mostly for backwards compatibility.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@
 module.exports.http = require('./http');
 module.exports.tcp = require('./tcp');
 
+// For backwards compatibility, and also because it's usualy what people want, pull in the HTTP hooks by default.
+module.exports._legion_hooks = module.exports.http._legion_hooks;
